fb26323a0eadd72510dd74fdae9d398ff1dede22
SophieD4B <sophie.delannoy@digital4better.com>
2021-03-04 17:11:44 +0100

    key code recongnition

--- src/components/key_exercise.vue
+++ src/components/key_exercise.vue
@@ -1,5 +1,6 @@
 <template>
-  <b-card class="exercise"
+  <b-card
+    class="exercise"
     bg-variant="secondary"
     header-bg-variant="dark"
     footer-bg-variant="dark"
@@ -7,15 +8,32 @@
     header="Exercice 1"
     footer-tag="footer"
     title="Appuie sur la touche :"
-    style="font-family: 'Tiresias Infofont', arial">
+    style="font-family: 'Tiresias Infofont', arial"
+  >
+    <link
+      href="https://fr.allfont.net/allfont.css?fonts=tiresias-infofont"
+      rel="stylesheet"
+      type="text/css"
+    />
 
-    <link href="https://fr.allfont.net/allfont.css?fonts=tiresias-infofont" rel="stylesheet" type="text/css" />
-
-    <section class=body>
-      <div><span class="key"> {{ key }}</span></div>
-      <div><input class="input" type="text" v-model="value" @keyup="checkKey" :disabled="success" v-focus></div>
+    <section class="body">
       <div>
-        <p v-if="error" class="is-error">Oups, tu t'es trompé(e) de touche, réessaie !</p>
+        <span class="key"> {{ key.name }}</span>
+      </div>
+      <div>
+        <input
+          class="input"
+          type="text"
+          v-model="value"
+          @keyup="checkKey"
+          :disabled="success"
+          v-focus
+        />
+      </div>
+      <div>
+        <p v-if="error" class="is-error">
+          Oups, tu t'es trompé(e) de touche, réessaie !
+        </p>
       </div>
       <div v-if="success">
         <p class="is-success">Bravo !</p>
@@ -24,10 +42,12 @@
     <template #footer>
       <div>
         <p class="errors-count" v-show="hasMadeOneError">
-          <strong>{{ keyErrors }}</strong> erreur a été commise pour la touche <strong>{{ key }}</strong>
+          <strong>{{ keyErrors }}</strong> erreur a été commise pour la touche
+          <strong>{{ key.name }}</strong>
         </p>
         <p class="errors-count" v-show="hasMadeErrors">
-          <strong>{{ keyErrors }}</strong> erreurs ont été commises pour la touche <strong>{{ key }}</strong>
+          <strong>{{ keyErrors }}</strong> erreurs ont été commises pour la
+          touche <strong>{{ key.name }}</strong>
         </p>
       </div>
       <div v-show="isEnd">
@@ -39,22 +59,35 @@
 </template>
 
 <script>
-import Clock from './clock.vue'
+import Clock from "./clock.vue"
 var synth = window.speechSynthesis
 
 export default {
   components: { Clock },
-  data () {
+  data() {
     return {
-      key: '',
-      value: '',
-      keys: ['Enter'],
+      key: "",
+      value: "",
+      keys: [
+        {
+          keyCode: 13,
+          name: "Entrée",
+        },
+        {
+          keyCode: 20,
+          name: "Verrouillage Majuscule",
+        },
+        {
+          keyCode: 17,
+          name: "Control",
+        },
+      ],
       attempts: 0, // nb total d'essais
       error: false,
       success: false,
       keyErrors: 0, // nb d'erreur par lettre
       totalErrors: 0, // nb d'erreur total
-      score: 0 // pourcentage de réussite
+      score: 0, // pourcentage de réussite
     }
   },
   mounted: function () {
@@ -62,30 +95,29 @@ export default {
     this.startWatch()
   },
   computed: {
-    hasMadeErrors () {
-      return (this.keyErrors > 1)
+    hasMadeErrors() {
+      return this.keyErrors > 1
     },
-    hasMadeOneError () {
-      return (this.keyErrors === 1)
+    hasMadeOneError() {
+      return this.keyErrors === 1
     },
-    isEnd () {
-      return (this.keys.length === 0 && this.success === true)
-    }
+    isEnd() {
+      return this.keys.length === 0 && this.success === true
+    },
   },
   methods: {
-    startWatch () {
+    startWatch() {
       this.$refs.clock.start()
     },
-    stopWatch () {
+    stopWatch() {
       this.$refs.clock.stop()
     },
-    checkKey (e) {
-      console.log(e)
-      this.speak(e.key)
+    checkKey(e) {
+      // this.speak(e.key)
       this.attempts++
-      if (e.key !== this.key) {
+      if (e.keyCode !== this.key.keyCode) {
         this.error = true
-        e.target.value = ''
+        e.target.value = ""
         this.keyErrors++
         this.totalErrors++
       } else {
@@ -98,79 +130,81 @@ export default {
         }
       }
       this.value = e.target.value
-      this.score = (((this.attempts - this.totalErrors) / this.attempts) * 100).toFixed(0)
+      this.score = (
+        ((this.attempts - this.totalErrors) / this.attempts) *
+        100
+      ).toFixed(0)
     },
-    initKey () {
+    initKey() {
       if (this.keys.length > 0) {
         this.key = this.keys.shift()
       }
-      this.speak(this.key)
+      this.speak(this.key.name)
     },
-    changeKey (e) {
+    changeKey(e) {
       this.keyErrors = 0
-      e.target.value = ''
-      this.value = ''
+      e.target.value = ""
+      this.value = ""
       if (this.keys.length > 0) {
         this.key = this.keys.shift()
-        this.speak('touche suivante :' + this.key)
+        this.speak("touche suivante :" + this.key.name)
       }
       this.success = false
       this.error = false
     },
-    async speak (oral) {
+    async speak(oral) {
       if (synth.speaking) {
         synth.cancel()
       }
-      if (oral !== '') {
+      if (oral !== "") {
         const utterThis = new SpeechSynthesisUtterance(oral)
         utterThis.onend = function (event) {
-          console.log('SpeechSynthesisUtterance.onend')
+          console.log("SpeechSynthesisUtterance.onend")
         }
         utterThis.onerror = function (event) {
-          console.error('SpeechSynthesisUtterance.onerror')
+          console.error("SpeechSynthesisUtterance.onerror")
         }
         utterThis.voice = this.$store.state.voice
         utterThis.pitch = this.$store.state.pitch
         utterThis.rate = this.$store.state.rate
         synth.speak(utterThis)
       }
-    }
+    },
   },
   directives: {
     focus: {
       inserted: function (el) {
         el.focus()
-      }
-    }
-  }
-}
+      },
+    },
+  },
+};
 </script>
 <style>
-
 .errors-count {
-text-align: left;
+  text-align: left;
 }
 
 .errors-count strong {
-font-weight: 600;
+  font-weight: 600;
 }
 
 .body {
-display: flex;
-flex-flow: wrap column;
-justify-content: flex-start;
-align-content: space-between;
+  display: flex;
+  flex-flow: wrap column;
+  justify-content: flex-start;
+  align-content: space-between;
 }
 
 .key {
-visibility: aria-hidden;
-font-size: 80px;
-background: rgba(0, 0, 0, 0.003);
+  visibility: aria-hidden;
+  font-size: 80px;
+  background: rgba(0, 0, 0, 0.003);
 }
 
-.input{
-visibility: aria-hidden;
-font-size: 80px;
-margin: 16px 16px 16px 0px
+.input {
+  visibility: aria-hidden;
+  font-size: 80px;
+  margin: 16px 16px 16px 0px;
 }
 </style>
