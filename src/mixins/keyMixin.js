4d92c24f61863533ccc0ef13ea3a14ee7fdec6be
= <=>
2022-12-05 14:24:23 +0100

    Fix eslint problems.

--- src/mixins/keyMixin.js
+++ src/mixins/keyMixin.js
@@ -1,61 +1,68 @@
 export default {
-  mounted: function () {
-    this.initContent()
-    this.speak(this.consigne)
-    this.initKey()
-    this.startWatch()
+  mounted: function() {
+    this.initContent();
+    this.speak(this.consigne);
+    this.initKey();
+    this.startWatch();
   },
   computed: {
-    hasMadeErrors () {
-      return this.keyErrors > 1
+    hasMadeErrors() {
+      return this.keyErrors > 1;
     },
-    hasMadeOneError () {
-      return this.keyErrors === 1
+    hasMadeOneError() {
+      return this.keyErrors === 1;
     },
-    isEnd () {
-      return this.keys.length === 0 && this.success === true
+    isEnd() {
+      return this.keys.length === 0 && this.success === true;
     }
   },
   methods: {
-    checkKey (e) {
-      console.log(e)
-      this.attempts++
+    checkKey(e) {
+      const keysToPreventDefault = [25];
+      if (keysToPreventDefault.includes(e.keyCode)) {
+        e.preventDefault();
+      }
+      console.log(e);
+      this.attempts++;
       if (e.keyCode !== this.key.keyCode) {
-        this.error = true
-        e.target.value = ''
-        this.keyErrors++
-        this.totalErrors++
+        this.error = true;
+        e.target.value = "";
+        this.keyErrors++;
+        this.totalErrors++;
       } else {
-        this.error = false
-        this.success = true
+        this.error = false;
+        this.success = true;
         if (this.keys.length !== 0) {
-          this.changeKey(e)
+          this.changeKey(e);
         } else {
-          this.congrat()
-          this.stopWatch()
+          this.congrat();
+          this.stopWatch();
         }
       }
-      this.value = e.target.value
-      this.score = (((this.attempts - this.totalErrors) / this.attempts) * 100).toFixed(0)
+      this.value = e.target.value;
+      this.score = (
+        ((this.attempts - this.totalErrors) / this.attempts) *
+        100
+      ).toFixed(0);
     },
-    initKey () {
-      this.success = false
-      this.error = false
+    initKey() {
+      this.success = false;
+      this.error = false;
       if (this.keys.length > 0) {
-        this.key = this.keys.shift()
+        this.key = this.keys.shift();
       }
-      setTimeout(() => this.speak(this.key.name), 4000)
+      setTimeout(() => this.speak(this.key.name), 4000);
     },
-    changeKey (e) {
-      this.keyErrors = 0
-      e.target.value = ''
-      this.value = ''
+    changeKey(e) {
+      this.keyErrors = 0;
+      e.target.value = "";
+      this.value = "";
       if (this.keys.length > 0) {
-        this.key = this.keys.shift()
-        setTimeout(() => this.speak(this.key.name), 600)
+        this.key = this.keys.shift();
+        setTimeout(() => this.speak(this.key.name), 600);
       }
-      this.success = false
-      this.error = false
+      this.success = false;
+      this.error = false;
     }
   }
-}
+};
