eb89c09c56beefc609e0bfa84a727eb7d29ef06b
= <=>
2022-12-07 16:57:17 +0100

    Fix clock starting in words exercices.

--- src/mixins/wordMixin.js
+++ src/mixins/wordMixin.js
@@ -1,92 +1,94 @@
-
 export default {
-  data () {
+  data() {
     return {
-      consigne: 'Saisis au clavier :',
-      word: '',
+      consigne: "Saisis au clavier :",
+      word: "",
       cursor: 0,
-      value: '',
-      previousValue: '',
+      value: "",
+      previousValue: "",
       attempts: 0, // nb total d'essais
       error: false,
       success: false,
       wordErrors: 0, // nb d'erreur par lettre
       totalErrors: 0, // nb d'erreur total
-      currentLetter: ''
-    }
+      currentLetter: ""
+    };
   },
-  created () {
-    this.speak(this.consigne)
-    this.initWord()
-    this.startWatch()
+  mounted: function() {
+    this.speak(this.consigne);
+    this.initWord();
+    this.startWatch();
   },
   computed: {
-    letters () {
-      return this.word.split('')
+    letters() {
+      return this.word.split("");
     },
-    hasMadeErrors () {
-      return (this.wordErrors > 1)
+    hasMadeErrors() {
+      return this.wordErrors > 1;
     },
-    hasMadeOneError () {
-      return (this.wordErrors === 1)
+    hasMadeOneError() {
+      return this.wordErrors === 1;
     },
-    isEnd () {
-      return (this.words.length === 0 && this.success === true)
+    isEnd() {
+      return this.words.length === 0 && this.success === true;
     },
-    score () {
-      return (((this.attempts - this.totalErrors) / this.attempts) * 100).toFixed(0)
+    score() {
+      return (
+        ((this.attempts - this.totalErrors) / this.attempts) *
+        100
+      ).toFixed(0);
     }
   },
   methods: {
-    checkWord (e) {
+    checkWord(e) {
       if (this.cursor > this.value.length) {
-        this.value = this.previousValue
-        return
+        this.value = this.previousValue;
+        return;
       }
-      this.attempts++
-      this.currentLetter = this.word[this.cursor]
+      this.attempts++;
+      this.currentLetter = this.word[this.cursor];
       if (this.value[this.cursor] === this.currentLetter) {
-        this.cursor++
-        this.previousValue = this.value
+        this.cursor++;
+        this.previousValue = this.value;
       } else {
-        this.value = this.previousValue
-        this.error = true
-        this.wordErrors++
-        this.totalErrors++
+        this.value = this.previousValue;
+        this.error = true;
+        this.wordErrors++;
+        this.totalErrors++;
       }
-      if (this.currentLetter === ' ') this.speakDelayed('Espace')
-      else this.speakDelayed(this.word[this.cursor])
+      if (this.currentLetter === " ") this.speakDelayed("Espace");
+      else this.speakDelayed(this.word[this.cursor]);
 
       if (this.value === this.word) {
-        this.error = false
-        this.success = true
+        this.error = false;
+        this.success = true;
         if (this.words.length !== 0) {
-          this.changeWord()
+          this.changeWord();
         } else {
-          this.congrat()
-          this.stopWatch()
+          this.congrat();
+          this.stopWatch();
         }
       }
     },
-    changeWord () {
-      this.nextWord()
-      setTimeout(() => this.speak(this.word), 3000)
-      setTimeout(() => this.speak(this.word[0]), 4000)
+    changeWord() {
+      this.nextWord();
+      setTimeout(() => this.speak(this.word), 3000);
+      setTimeout(() => this.speak(this.word[0]), 4000);
     },
-    initWord () {
-      this.nextWord()
-      setTimeout(() => this.speak(this.word), 3500)
-      setTimeout(() => this.speak(this.word[0]), 5000)
+    initWord() {
+      this.nextWord();
+      setTimeout(() => this.speak(this.word), 3500);
+      setTimeout(() => this.speak(this.word[0]), 5000);
     },
-    nextWord () {
-      this.wordErrors = 0
+    nextWord() {
+      this.wordErrors = 0;
       if (this.words.length > 0) {
-        this.word = this.words.shift()
+        this.word = this.words.shift();
       }
-      this.success = false
-      this.error = false
-      this.cursor = 0
-      this.previousValue = this.value = ''
+      this.success = false;
+      this.error = false;
+      this.cursor = 0;
+      this.previousValue = this.value = "";
     }
   }
-}
+};
