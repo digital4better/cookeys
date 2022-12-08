b53f8a0f45c47b3e3be7d016d320ba6187f9c9d6
= <=>
2022-12-08 15:36:05 +0100

    Fix punctuaton pronounciation.

--- src/mixins/speakMixin.js
+++ src/mixins/speakMixin.js
@@ -1,30 +1,47 @@
-var synth = window.speechSynthesis
+var synth = window.speechSynthesis;
 
 export default {
   methods: {
-    speak (oral) {
+    mapPunctuation(value) {
+      switch (value) {
+        case ",":
+          return "Virgule";
+        case ".":
+          return "Point";
+        case "!":
+          return "Point d'exclamation";
+        case "?":
+          return "Point d'interrogation";
+        case "'":
+          return "Apostrophe";
+        default:
+          return value;
+      }
+    },
+    speak(oral) {
       if (synth.speaking) {
-        synth.cancel()
+        synth.cancel();
       }
-      if (oral !== '') {
-        const utterThis = new SpeechSynthesisUtterance(oral)
-        utterThis.onend = function (event) {
-          console.log('SpeechSynthesisUtterance.onend')
-        }
-        utterThis.onerror = function (event) {
-          console.error('SpeechSynthesisUtterance.onerror')
-        }
-        utterThis.voice = this.$store.state.voice
-        utterThis.pitch = this.$store.state.pitch
-        utterThis.rate = this.$store.state.rate
-        synth.speak(utterThis)
+      if (oral !== "") {
+        let utterThis;
+        utterThis = new SpeechSynthesisUtterance(this.mapPunctuation(oral));
+        utterThis.onend = function(event) {
+          console.log("SpeechSynthesisUtterance.onend");
+        };
+        utterThis.onerror = function(event) {
+          console.error("SpeechSynthesisUtterance.onerror");
+        };
+        utterThis.voice = this.$store.state.voice;
+        utterThis.pitch = this.$store.state.pitch;
+        utterThis.rate = this.$store.state.rate;
+        synth.speak(utterThis);
       }
     },
-    stopSpeech () {
-      synth.cancel()
+    stopSpeech() {
+      synth.cancel();
     },
-    speakDelayed (oral) {
-      setTimeout(() => this.speak(oral), 1100)
+    speakDelayed(oral) {
+      setTimeout(() => this.speak(oral), 1100);
     }
   }
-}
+};
