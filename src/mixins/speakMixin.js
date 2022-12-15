a769548d7810ef72f76e5c4cff0580bd3fa88280
= <=>
2022-12-15 16:11:26 +0100

    Fix Sonar alert.

--- src/mixins/speakMixin.js
+++ src/mixins/speakMixin.js
@@ -6,61 +6,34 @@ export default {
       if (/^[A-ZÉ]$/.test(value)) {
         return value + ' ' + 'majuscule'
       }
-      switch (value) {
-        case ',':
-          return 'Virgule'
-        case '.':
-          return 'Point'
-        case '!':
-          return "Point d'exclamation"
-        case '?':
-          return "Point d'interrogation"
-        case "'":
-          return 'Apostrophe'
-        case ' ':
-          return 'Espace'
-        case ':':
-          return 'Deux points'
-        case ';':
-          return 'Point virgule'
-        case '-':
-          return "Trait d'union"
-        case '(':
-          return 'Parenthèse gauche'
-        case ')':
-          return 'Parenthèse droite'
-        case '[':
-          return 'crochet gauche'
-        case ']':
-          return 'Crochet droit'
-        case '{':
-          return 'Accolade gauche'
-        case '}':
-          return 'Accolade droite'
-        case '"':
-          return 'Guillemet'
-        case 'è':
-          return 'E accent grave'
-        case 'ê':
-          return 'E accent circonflexe'
-        case 'ô':
-          return 'O accent circonflexe'
-        case 'â':
-          return 'A accent grave'
-        case 'é':
-          return 'E accent aigu'
-        case 'Î':
-          return 'I accent circonflexe'
-        case 'ë':
-          return 'E tréma'
-        case 'à':
-          return 'A accent grave'
-        case 'ç':
-          return 'C cédille'
-
-        default:
-          return value
+      const pronunciation = new Map()
+      pronunciation.set(',', 'Virgule')
+      pronunciation.set('.', 'Point')
+      pronunciation.set('!', "Point d'exclamation")
+      pronunciation.set('?', 'Point dinterrogation')
+      pronunciation.set("'", 'Apostrophe')
+      pronunciation.set(':', 'Deux points')
+      pronunciation.set('(', 'Parenthèse gauche')
+      pronunciation.set(')', 'Parenthèse droite')
+      pronunciation.set('[', 'Crochet gauche')
+      pronunciation.set(']', 'Crochet droit')
+      pronunciation.set('{', 'Accolade gauche')
+      pronunciation.set('}', 'Accolade droite')
+      pronunciation.set('"', 'Guillemet')
+      pronunciation.set('-', "Trait d'union")
+      pronunciation.set('è', 'E accent grave')
+      pronunciation.set('ê', 'E accent circonflexe')
+      pronunciation.set('ô', 'O circonflexe')
+      pronunciation.set('â', 'A accent circonflexe')
+      pronunciation.set('é', 'E accent aigu')
+      pronunciation.set('î', 'I accent circonflexe')
+      pronunciation.set('ë', 'E tréma')
+      pronunciation.set('à', 'A accent grave')
+      pronunciation.set(' ', 'Espace')
+      if (pronunciation.has(value)) {
+        return pronunciation.get(value)
       }
+      return value
     },
     speak (oral) {
       if (synth.speaking && this.$store.state.oralPreview.length === 1) {
